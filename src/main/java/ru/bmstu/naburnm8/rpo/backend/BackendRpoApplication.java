a6a3209a3c670336f5b4b75e54ebaf51d54a2cfd
naburnm8 <alint@bk.ru>
2025-03-04 23:42:37 +0300

    part4: DONE

---
+++ src/main/java/ru/bmstu/naburnm8/rpo/backend/BackendRpoApplication.java
@@ -0,0 +1,13 @@
+package ru.bmstu.naburnm8.rpo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRpoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendRpoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bmstu/naburnm8/rpo/backend/BackendRpoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bmstu.naburnm8.rpo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRpoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
